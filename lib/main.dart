14665c9029184dbaf49e10116952f338187d4c06
tt <ttdyce@gmail.com>
2024-03-13 02:53:41 +0800

    add trailing button for demo

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 /// Flutter code sample for pinned [SearchAnchor] while scrolling.
 
 void main() {
-  runApp(const PinnedSearchBarApp());
+  runApp(MaterialApp(
+    theme:
+        ThemeData(useMaterial3: true, colorSchemeSeed: const Color(0xff6750a4)),
+    home: const PinnedSearchBarApp(),
+  ));
 }
 
 class PinnedSearchBarApp extends StatefulWidget {
@@ -16,22 +20,19 @@ class PinnedSearchBarApp extends StatefulWidget {
 class _PinnedSearchBarAppState extends State<PinnedSearchBarApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          useMaterial3: true, colorSchemeSeed: const Color(0xff6750a4)),
-      home: Scaffold(
-        body: SafeArea(
-          child: CustomScrollView(
-            slivers: <Widget>[
-              SliverAppBar(
-                clipBehavior: Clip.none,
-                shape: const StadiumBorder(),
-                scrolledUnderElevation: 0.0,
-                titleSpacing: 0.0,
-                backgroundColor: Colors.transparent,
-                floating:
-                    true, // We can also uncomment this line and set `pinned` to true to see a pinned search bar.
-                title: SearchAnchor.bar(
+    return Scaffold(
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              clipBehavior: Clip.none,
+              shape: const StadiumBorder(),
+              scrolledUnderElevation: 0.0,
+              titleSpacing: 0.0,
+              backgroundColor: Colors.transparent,
+              floating:
+                  true, // We can also uncomment this line and set `pinned` to true to see a pinned search bar.
+              title: SearchAnchor.bar(
                   suggestionsBuilder:
                       (BuildContext context, SearchController controller) {
                     return List<Widget>.generate(
@@ -44,41 +45,57 @@ class _PinnedSearchBarAppState extends State<PinnedSearchBarApp> {
                       },
                     );
                   },
-                ),
-              ),
-              // The listed items below are just for filling the screen
-              // so we can see the scrolling effect.
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: SizedBox(
-                    height: 100.0,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 10,
-                      itemBuilder: (BuildContext context, int index) {
-                        return SizedBox(
-                          width: 100.0,
-                          child: Card(
-                            child: Center(child: Text('Card $index')),
-                          ),
-                        );
+                  barTrailing: [
+                    IconButton.filledTonal(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (context) => const AlertDialog(
+                                  title: Text("Title"),
+                                  content: Text("Content"),
+                                ));
                       },
+                      icon: ClipOval(
+                        child: Image.network(
+                          "https://www.gravatar.com/avatar/b004c065bc529e98545e27af859152bb74007e535f2c149284117cfb520e76d6?d=retro&f=y",
+                          height: 24,
+                        ),
+                      ),
                     ),
+                  ]),
+            ),
+            // The listed items below are just for filling the screen
+            // so we can see the scrolling effect.
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: SizedBox(
+                  height: 100.0,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 10,
+                    itemBuilder: (BuildContext context, int index) {
+                      return SizedBox(
+                        width: 100.0,
+                        child: Card(
+                          child: Center(child: Text('Card $index')),
+                        ),
+                      );
+                    },
                   ),
                 ),
               ),
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Container(
-                    height: 1000,
-                    color: Colors.deepPurple.withOpacity(0.5),
-                  ),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Container(
+                  height: 1000,
+                  color: Colors.deepPurple.withOpacity(0.5),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
